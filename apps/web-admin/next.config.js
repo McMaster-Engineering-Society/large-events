03b5e1c14e0df4bac15569a33b8cfe6f7f8563e3
Luke <schuurml@mcmaster.ca>
2025-11-09 09:17:18 -0500

    Clean-up legacy next.js and nest.js

--- apps/web-admin/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    API_GATEWAY_URL: process.env.API_GATEWAY_URL || 'http://localhost:3000',
-  },
-};
-
-module.exports = nextConfig;
